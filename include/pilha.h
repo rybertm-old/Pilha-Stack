0d9fe00a531101e5d5fba879a866225dd97152d8
rybertm <bitmoraes@hotmail.com>
2019-03-27 06:46:13 -0300

    Header Files

---
+++ include/pilha.h
@@ -0,0 +1,25 @@
+﻿#ifndef PILHA_H
+#define PILHA_H
+
+template <typename T>
+class Pilha
+{
+private:
+	T *m_dado;
+	int m_top;
+	int m_tam;
+
+public:
+	Pilha(int tam);
+	~Pilha();
+
+	bool isFull();
+	bool isEmpty();
+	bool push(T dado);
+	T	 pop();
+	T	 top();
+	void empty();
+	void print();
+};
+
+#endif	//PILHA_H
